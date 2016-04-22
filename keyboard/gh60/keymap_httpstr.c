8b47f3818d9c9c08ed30dc051fb409aef2534833
Eli Gladman <eli.gladman@gmail.com>
2016-04-22 05:54:15 -0400

    implemented oneshot modifier

--- keyboard/gh60/keymap_httpstr.c
+++ keyboard/gh60/keymap_httpstr.c
@@ -8,7 +8,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
     LGUI,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          FN9, \
-    LCTL,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
+    FN10,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
 
   /* 1: colemak */
   KEYMAP_ANSI(
@@ -16,7 +16,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS, \
     BSPC,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,  \
     LSFT,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,          FN9, \
-    LCTL,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
+    FN10,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
 
   /* 2: dvorak */
   KEYMAP_ANSI(
@@ -24,7 +24,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS, \
     LGUI,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     ENT,  \
     LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,             FN9, \
-    LCTL,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
+    FN10,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
 
   /* 3: workman */
   KEYMAP_ANSI(
@@ -32,7 +32,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TAB, Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,BSLS, \
       LGUI,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,     ENT,  \
       LSFT,Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,          FN9, \
-      LCTL,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
+      FN10,FN0,LALT,          FN8,                     RALT, FN2, FN1, FN0),
 
   /* 4: Primary Function Layer */
   KEYMAP_ANSI(
@@ -72,4 +72,5 @@ const uint16_t PROGMEM fn_actions[] = {
 
   [8] = ACTION_LAYER_TAP_KEY(4, KC_SPC),
   [9] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_GRV),
+  [10] = ACTION_MODS_ONESHOT(MOD_LCTL),
 };
