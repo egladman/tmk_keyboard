1cd7ff598adca029291faec01e985eb3a7c36850
Eli Gladman <eli.gladman@gmail.com>
2016-04-23 20:23:34 -0400

    changed behavior when right shift is tapped

--- keyboard/hhkb/keymap_httpstr.c
+++ keyboard/hhkb/keymap_httpstr.c
@@ -15,7 +15,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
     LGUI,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
+    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN9,FN0,             \
          FN10,LALT,          FN8,                FN2, FN1),
 
   /* 1: colemak */
